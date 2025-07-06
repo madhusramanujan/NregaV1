4a3534a16ef0132896a71d63944e575dbb7a2dae
madhusramanujan <madhu.sramanujan@gmail.com>
2025-07-06 11:06:57 +0530

    added index header and footer

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+</main>
+<footer class="bg-dark text-white text-center mt-auto p-3">
+  &copy; <?php echo date("Y"); ?> NREGA Portal. All rights reserved.
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include 'includes/header.php'; ?>
+<div class="text-center">
+  <h2 class="mb-4">Welcome to NREGA Activity Reporting Portal</h2>
+  <p>Please select your login type:</p>
+  <div class="d-grid gap-3 col-6 mx-auto mt-4">
+    <a href="userLogin.php" class="btn btn-primary btn-lg">User Login</a>
+    <a href="adminLogin.php" class="btn btn-primary btn-lg">Admin Login</a>
+  </div>
+</div>
+<?php include 'includes/footer.php'; ?>
